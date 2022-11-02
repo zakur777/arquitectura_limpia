146bccfeb028a2072cffeaea9f10dd7ce6b697e6
zakur <walter.barra@outlook.com>
2022-11-02 20:55:15 -0300

    Clean Architecture Multimodule - Demo

---
+++ app-demo-arq-clean-multi-modulo/app-demo-arq-clean/app-demo-arq-clean-app/src/main/java/pe/edu/galaxy/training/java/arq/clean/app/AppDemo.java
@@ -0,0 +1,13 @@
+package pe.edu.galaxy.training.java.arq.clean.app;
+
+import pe.edu.galaxy.training.java.arq.clean.bean.Demo;
+
+public class AppDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println(new Demo().demo("Aristedes"));
+
+	}
+
+}
---
+++ app-demo-arq-clean/app-demo-arq-clean-bean/src/main/java/pe/edu/galaxy/training/java/arq/clean/bean/Demo.java
@@ -0,0 +1,9 @@
+package pe.edu.galaxy.training.java.arq.clean.bean;
+
+public class Demo {
+
+	public String demo(String nombre) {
+		return "Hola "+nombre.toUpperCase();
+	}
+	
+}
